752f335dbe8222b726d3cca0ce68905492854166
Said Maadan <saidmaadan@gmail.com>
2014-07-06 14:49:12 -0500

    Redesigned of project, user, idea view pages

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,6 +4,16 @@ class Idea < ActiveRecord::Base
   validates :title, :goal, :category, :market, presence: true
 
   validates :description, length: { minimum: 25 }
+ 
+  CATEGORY = ['WEB APP', 'MOBILE APP', "IOS", "ANDROID", "BLACKBERRY", "ANDROID, WINDOW APP, IOS", "ALL PLATFORMS"]
+   validates :category, inclusion: { in: CATEGORY }
+
+   MARKET = ['GLOBAL MARKET', 'US MARKET', "EUROPEAN MARKET", "AFRICAN MARKET ", "ASIAN MARKET", "OTHER"]
+   validates :market, inclusion: { in: MARKET }
+
+   GOAL = ['FREE STATRTUP IDEA POST', 'LOOKING FOR DEVELOPER TO WORK ON THIS PROJECT', 'PARTNERSHIP TO WORK ON THIS IDEA', "OTHER"]
+   validates :market, inclusion: { in: MARKET }
+
 
 def self.recent
     order("created_at desc")
