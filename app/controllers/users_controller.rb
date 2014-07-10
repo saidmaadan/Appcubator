eea7fac9872c9dc95d3ac429087b454dfe91d185
Said Maadan <saidmaadan@gmail.com>
2014-07-09 23:11:21 -0500

    Modified user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: [:new, :create]
   before_action :require_correct_user, only: [:edit, :update, :destroy]
-
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.all
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "You have successfully sign up!"
     else
       render :new
