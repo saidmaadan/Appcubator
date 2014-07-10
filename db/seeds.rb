2ef48bd34ed02a2f2a3ebac6b430932b4e21257c
Said Maadan <saidmaadan@gmail.com>
2014-07-09 23:30:05 -0500

    Add db/seeds.rb to run rake on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Project.create!([
-#     {
-#        name: "Web Game",
-#        description: "Search game for everything on the web",
-#        teams: "Wale Alex, Ted Scars",
-#        looking_for: "Partnership",
-#        target_amount: 800000
-#      },
-#      {
-#        name: "Restaurant Locator",
-#        description: "Restourant locator mobile application",
-#        teams: "Ben Okaf, Maltida Williamx",
-#        looking_for: "Buyer",
-#        target_amount: 9500000
-#      },
-#      {
-#        name: "Family Social",
-#        description: "Family private social network that connect",
-#        teams: "NiKKo Wee, Ted Ibrah, Alve Rhino",
-#        looking_for: "Partnership",
-#        target_amount: 5500000
-#      }
+Project.create!([
+    {
+       name: "Web Game",
+       description: "Search game for everything on the web",
+       teams: "Wale Alex, Ted Scars",
+       looking_for: "Partnership",
+       target_amount: 800000
+     },
+     {
+       name: "Restaurant Locator",
+       description: "Restourant locator mobile application",
+       teams: "Ben Okaf, Maltida Williamx",
+       looking_for: "Buyer",
+       target_amount: 9500000
+     },
+     {
+       name: "Family Social",
+       description: "Family private social network that connect",
+       teams: "NiKKo Wee, Ted Ibrah, Alve Rhino",
+       looking_for: "Partnership",
+       target_amount: 5500000
+     }
 
-#      ])
+     ])
 
 # idea = Idea.first
 # idea.checkins.create(name: "Bobby S", email: "bobby@bobby.com", skills: "Backend", experience: 3, position: 'Part time', comment: "Tell me more about the project")
 
- # strenghts = [" HTML ", "CSS ", " RUBY", " JAVASCRIPT", " JAVA", " C", " C++", " C#", " CLOJURE", " PYTHON", " PERL", " SCALA", " COFFEE-SCRIPT", " HASKELL", "  PHP", " ADA", " ATS", " RUBY on RAILS", " BACKBONE", " EMBER", " ANGULAR", " NODE", " JQUERY", " RIOT", " GIT VERSION CONTROL", " TDD", " CSS FRAMEWORK", " MYSQL", " POSTGRES", " SQLITE3" ]
- # strenghts.each { |strenght| Ability.create!(strenght:strenght) }
+ strenghts = [" HTML ", "CSS ", " RUBY", " JAVASCRIPT", " JAVA", " C", " C++", " C#", " CLOJURE", " PYTHON", " PERL", " SCALA", " COFFEE-SCRIPT", " HASKELL", "  PHP", " ADA", " ATS", " RUBY on RAILS", " BACKBONE", " EMBER", " ANGULAR", " NODE", " JQUERY", " RIOT", " GIT VERSION CONTROL", " TDD", " CSS FRAMEWORK", " MYSQL", " POSTGRES", " SQLITE3" ]
+ strenghts.each { |strenght| Ability.create!(strenght:strenght) }
 
