d6f1a5a02b56120fbe295e316fc375465e4fbc28
Said Maadan <saidmaadan@gmail.com>
2014-08-04 11:53:28 -0500

    modified user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :password, length: { minimum: 8, allow_blank: true }
+  # validates :password, length: { minimum: 8, allow_blank: true }
 
-  validates :name, presence: true
-  validates :email, presence: true,
-                  format: /\A\S+@\S+\z/,
-                  uniqueness: { case_sensitive: false }
+  # validates :name, presence: true
+  # validates :email, presence: true,
+  #                 format: /\A\S+@\S+\z/,
+  #                 uniqueness: { case_sensitive: false }
 
-  validates :username, presence: true,
-                     format: /\A[A-Z0-9]+\z/i,
-                     uniqueness: { case_sensitive: false }
+  # validates :username, presence: true,
+  #                    format: /\A[A-Z0-9]+\z/i,
+  #                    uniqueness: { case_sensitive: false }
 
   SEX = ['Male', 'Female', "I'll rather not say"]
   #validates :sex, inclusion: { in: SEX }
