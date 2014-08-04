69bc5d6608a311b1ee00e60df0e0a961801049f6
Said Maadan <saidmaadan@gmail.com>
2014-08-04 13:12:27 -0500

    Delete omniauth file

--- config/initializers/omniauth.rb
+++
@@ -1,5 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '306201809551361', '78259e03ab25b96aef8ddf72ce306f61', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
-end
