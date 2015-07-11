8f17f757b3f000bb05c3680c003a8acc66c90c9b
Said Maadan <saidmaadan@users.noreply.github.com>
2015-07-10 22:39:05 -0500

    Added styles to home page view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
   	@projects = Project.all.order("created_at DESC").limit(4)
-  	 @ideas = Idea.recent.limit(4)
-  	 @jobs = Job.recent.limit(3)
+  	 @ideas = Idea.recent.limit(2)
+  	 @jobs = Job.all.limit(4)
   	 @trends = Trend.recent.limit(1)
   	 @user = User.new
   end
